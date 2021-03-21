35531995df6ef2e359c4a46e7de556b5208f1412
alchristophergwapo <alchristopheroppa0143@gmail.com>
2021-03-21 10:52:42 +0800

    Updates 03/21

--- app/Events/StudentEnrollEvent.php
+++ app/Events/StudentEnrollEvent.php
@@ -10,7 +10,7 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class StudentEnrollEvent implements ShouldBroadcast
+class StudentEnrollEvent
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
