ba2bf7a6d371931a7b1d854bd70bbb7a541d25b6
MissQAssociates <msqintern0@gmail.com>
2021-03-27 21:18:37 +0800

    Updates 03/27

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -29,47 +29,47 @@ class DatabaseSeeder extends Seeder
         ]);
 
         // Section::factory()->count(50)->create();
-
+        Teacher::factory()->count(50)->create();
         $students = Student::factory()->count(50)->create();
         foreach($students as $student) {
             Enrollment::create([
                 'start_school_year' => 2020,
                 'end_school_year' => 2021,
-                'enrollment_status' => 'Approved',
+                'enrollment_status' => 'Pending',
                 'student_id' =>$student->id,
                 'card_image' => '1616211904451_WIN_20210311_14_56_12_Pro.jpg',
             ]);
 
-            User::create([
-                'user_type' => 'student',
-                'username' => $student->LRN,
-                'password' => \Hash::make($student->lastname.$student->LRN)
-            ]);
+            // User::create([
+            //     'user_type' => 'student',
+            //     'username' => $student->LRN,
+            //     'password' => \Hash::make($student->lastname.$student->LRN)
+            // ]);
 
         };
 
-        // $grade_levels = [
-        //         [
-        //             'grade_level' => 7
-        //         ],
-        //         [
-        //             'grade_level' => 8
-        //         ],
-        //         [
-        //             'grade_level' => 9
-        //         ],
-        //         [
-        //             'grade_level' => 10
-        //         ],
-        //         [
-        //             'grade_level' => 11
-        //         ],
-        //         [
-        //             'grade_level' => 12
-        //         ]
-        //     ];
-        // foreach($grade_levels as $grade_level){
-        //     GradeLevel::create($grade_level);
-        // }
+        $grade_levels = [
+                [
+                    'grade_level' => 7
+                ],
+                [
+                    'grade_level' => 8
+                ],
+                [
+                    'grade_level' => 9
+                ],
+                [
+                    'grade_level' => 10
+                ],
+                [
+                    'grade_level' => 11
+                ],
+                [
+                    'grade_level' => 12
+                ]
+            ];
+        foreach($grade_levels as $grade_level){
+            GradeLevel::create($grade_level);
+        }
     }
 }
