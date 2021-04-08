57de17e9375347564b5e4a5c16cb4adafd82246f
mae-yosores2k19 <jessamaehortadoyosores@gmail.com>
2021-04-08 13:06:53 +0800

    Updates 04/08

--- app/Http/Controllers/SubjectController.php
+++ app/Http/Controllers/SubjectController.php
@@ -12,83 +12,86 @@ use App\Models\Subject;
 use App\Models\GradeLevel;
 use App\Models\Teacher;
 
+use function React\Promise\Stream\first;
+
 class SubjectController extends Controller
 {
     public function allSubjectsByGrLevel($grade)
     {
         $gradelevel = GradeLevel::where('grade_level', '=', $grade)->first();
-        $subjects = Subject::where('grade_level_id', '=', $gradelevel->id)
-            ->join('teachers', 'subjects.teacher_id', 'teachers.id')
-            ->select('subjects.*', 'teachers.teacher_name')
-            ->get();
+        if ($gradelevel) {
+            $subjects = \DB::table('subjects')->where('subjects.grade_level_id', '=', $gradelevel->id)
+                ->join('teachers', 'subjects.teacher_id', 'teachers.id')
+                ->select('subjects.*', 'teachers.teacher_name')
+                ->get();
 
-        return response(['subjects' => $subjects]);
+            return response(['subjects' => $subjects]);
+        }
     }
 
-    public function addSubjectInGrLevel(Request $request)
+    public function addSubjectInGrLevel(SubjectRequest $request)
     {
-        $datas = $request->all();
-        $added = 0;
-        // return response(count($data));
-        foreach ($datas as $data) {
-            $gradelevel = GradeLevel::where(
-                'grade_level',
-                '=',
-                $data['grade_level_id']
-            )->first();
-            // return response($gradelevel);
-            // $teacher = Teacher::where(
-            //     'teacher_name',
-            //     $data['teacher_id']
-            // )->first();
-            $subOnDb = Subject::where('subject_name', '=', $data['name'])
-                ->where('grade_level_id', '=', $gradelevel->id)
-                ->first();
+        $data = $request->all();
+        $gradelevel = GradeLevel::where(
+            'grade_level',
+            '=',
+            $data['grade_level_id']
+        )->first();
+        $subOnDb = Subject::where('subject_name', '=', $data['subject_name'])
+            ->where('grade_level_id', '=', $gradelevel->id)
+            ->first();
 
-            if ($subOnDb == null) {
-                $newRequest = [
-                    'subject_name' => $data['name'],
-                    'teacher_id' => $data['teacher_id'],
-                    'grade_level_id' => $gradelevel->id,
-                ];
+        if ($subOnDb == null) {
+            $validated = $request->validated();
 
-                $validated = Validator::make($newRequest, [
-                    'subject_name' => [
-                        'required',
-                        'string',
-                        'min:2',
-                        'max:255',
-                    ],
-                    'teacher_id' => ['required'],
-                    'grade_level_id' => ['required', 'integer'],
-                ])->validate();
-
-                if ($validated) {
-                    try {
-                        \DB::beginTransaction();
-                        $subject = Subject::create($validated);
-                        \DB::commit();
-                        $added += 1;
-                    } catch (\Exception $e) {
-                        \DB::rollback();
-                        return response(['error' => $e->getMessage()], 500);
-                    }
+            if ($validated) {
+                try {
+                    \DB::beginTransaction();
+                    Subject::create($validated);
+                    // Teacher::where('id','=',$validated->teacher_id)
+                    // ->update([
+                    //     'subjects_id'
+                    // ])
+                    \DB::commit();
+                } catch (\Exception $e) {
+                    \DB::rollback();
+                    return response(['error' => $e->getMessage()], 500);
                 }
-            } else {
-                return response(
-                    [
-                        'error' =>
-                            $data['name'] .
-                            ' already exist on Grade ' .
-                            $gradelevel->grade_level,
-                    ],
-                    400
-                );
             }
         }
+    }
 
-        if ($added == count($datas)) {
-            return response(['success' => 'Added Successfully.']);
+    public function updateSubject(SubjectRequest $request)
+    {
+        $subject = Subject::where('id', '=', $request->id)
+            ->first();
+
+        try {
+            \DB::beginTransaction();
+            $teacher = \DB::table('teachers')->where('teachers.id', '=', $request->teacher_id)
+                ->join('subjects', 'teachers.id', 'subjects.teacher_id')
+                ->select('teachers.*', 'subjects.subject_name')
+                ->first();
+            if (!$teacher) {
+                $validated = $request->validated();
+
+                if ($validated) {
+                    $subject->update($validated);
+                }
+                \DB::commit();
+                return response(['success' => 'Successfully updated.']);
+            } else {
+                return response(['failed' => $teacher->teacher_name . ' is already assigned on ' . $teacher->subject_name], 422);
+            }
+        } catch (\Exception $e) {
+            \DB::rollBack();
+            return response(['error' => $e->getMessage()]);
         }
+    }
+
+    public function deleteSubject($id)
+    {
+        Subject::where('id', $id)->delete();
+        return response(['success' => 'Successfully deleted!']);
     }
 }
