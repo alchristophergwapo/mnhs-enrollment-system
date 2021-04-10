7cf14ff2d849d54f88ff1a865371ffefed2831ed
mae-yosores2k19 <jessamaehortadoyosores@gmail.com>
2021-04-10 17:13:47 +0800

    Updates 04/10

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,56 +20,55 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-       
-        User::create([
-            'user_type' => 'admin',
-            'username' => 'Administrator',
-            'password' => \Hash::make('Password'),
-            'remember_token' => \Str::random(10),
-        ]);
-            Teacher::factory()->count(50)->create();
+
+        // User::create([
+        //     'user_type' => 'admin',
+        //     'username' => 'Administrator',
+        //     'password' => \Hash::make('Password'),
+        //     'remember_token' => \Str::random(10),
+        // ]);
+        // Teacher::factory()->count(50)->create();
         // Section::factory()->count(50)->create();
 
-        $students = Student::factory()->count(50)->create();
-        foreach($students as $student) {
+        $students = Student::factory()->count(100)->create();
+        foreach ($students as $student) {
             Enrollment::create([
-                'start_school_year' => 2020,
-                'end_school_year' => 2021,
-                'enrollment_status' => 'Pending',
-                'student_id' =>$student->id,
+                'start_school_year' => 2022,
+                'end_school_year' => 2023,
+                'enrollment_status' => 'Approved',
+                'student_id' => $student->id,
                 'card_image' => '1616211904451_WIN_20210311_14_56_12_Pro.jpg',
             ]);
 
-        //     User::create([
-        //         'user_type' => 'student',
-        //         'username' => $student->LRN,
-        //         'password' => \Hash::make($student->lastname.$student->LRN)
-        //     ]);
-
+            User::create([
+                'user_type' => 'student',
+                'username' => $student->LRN,
+                'password' => \Hash::make($student->lastname . $student->LRN)
+            ]);
         };
 
-        $grade_levels = [
-                [
-                    'grade_level' => 7
-                ],
-                [
-                    'grade_level' => 8
-                ],
-                [
-                    'grade_level' => 9
-                ],
-                [
-                    'grade_level' => 10
-                ],
-                [
-                    'grade_level' => 11
-                ],
-                [
-                    'grade_level' => 12
-                ]
-            ];
-        foreach($grade_levels as $grade_level){
-            GradeLevel::create($grade_level);
-        }
+        // $grade_levels = [
+        //     [
+        //         'grade_level' => 7
+        //     ],
+        //     [
+        //         'grade_level' => 8
+        //     ],
+        //     [
+        //         'grade_level' => 9
+        //     ],
+        //     [
+        //         'grade_level' => 10
+        //     ],
+        //     [
+        //         'grade_level' => 11
+        //     ],
+        //     [
+        //         'grade_level' => 12
+        //     ]
+        // ];
+        // foreach ($grade_levels as $grade_level) {
+        //     GradeLevel::create($grade_level);
+        // }
     }
 }
