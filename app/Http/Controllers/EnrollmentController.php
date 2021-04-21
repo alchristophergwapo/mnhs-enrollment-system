60c15194b012787614dee626870674564d600cd2
mae-yosores2k19 <jessamaehortadoyosores@gmail.com>
2021-04-21 17:16:26 +0800

    Updated 04/21

--- app/Http/Controllers/EnrollmentController.php
+++ app/Http/Controllers/EnrollmentController.php
@@ -329,7 +329,7 @@ class EnrollmentController extends Controller
                     ]);
                     $enrollment->update([
                         'enrollment_status' => 'Approved',
-                        'student_section' => $section->name,
+                        'student_section' => $section->id,
                     ]);
                     \DB::commit();
 
