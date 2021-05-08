506890831ae07d00ca7f670078071e020bf45f0c
alchristophergwapo <alchristopheroppa0143@gmail.com>
2021-05-08 17:28:26 +0800

    Updated 0508

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -77,18 +77,30 @@ class AdminController extends Controller
 
         try {
             \DB::beginTransaction();
-            if ($editAccountValidated) {
-                User::where('id', '=', $id)->update($editAccountValidated);
-                UserDetails::where('user_id', $id)->update([
-                    'email' => $request->user_email,
-                    'user_fullname' => $request->user_fullname
-                ]);
-                \DB::commit();
-                return response(['success' => "Account Updated Successfully.", $editAccountValidated]);
+            $admin = User::where('username', $request->username)->first();
+            $admin_details = UserDetails::where('user_fullname', $request->user_fullname)
+                ->leftJoin('users', 'users.id', 'user_details.user_id')
+                ->select('users.username', 'user_details.user_fullname')
+                ->first();
+            if ($admin) {
+                return response(['teacher_admin_exist' => 'An admin for grade ' . explode("_", $request->username)[1] . ' already exist.', "teacher_admin" => $admin], 400);
+            }
+            if ($admin_details) {
+                return response(['teacher_isAssigned' => $admin_details->user_fullname . ' is already assigned to grade ' . explode("_", $admin_details->username)[1]], 400);
             } else {
-                \DB::rollBack();
-                return response(['error' => 'Something went wrong!'], 400);
-            };
+                if ($editAccountValidated) {
+                    User::where('id', '=', $id)->update($editAccountValidated);
+                    UserDetails::where('user_id', $id)->update([
+                        'email' => $request->user_email,
+                        'user_fullname' => $request->user_fullname
+                    ]);
+                    \DB::commit();
+                    return response(['success' => "Account Updated Successfully.", $editAccountValidated]);
+                } else {
+                    \DB::rollBack();
+                    return response(['error' => 'Something went wrong!'], 400);
+                };
+            }
         } catch (\Exception $e) {
             \DB::rollBack();
             return response(['error' => $e->getMessage()], 500);
