dc84d84e0e1df072ba8fb9d951b39d955449cdea
alchristophergwapo <alchristopheroppa0143@gmail.com>
2021-05-13 19:46:43 +0800

    updated

--- app/Http/Requests/TeacherRequest.php
+++ app/Http/Requests/TeacherRequest.php
@@ -25,7 +25,7 @@ class TeacherRequest extends FormRequest
     {
         //'unique:teachers,name'
         return [
-            'teacher_name' => ['required', 'string', 'min:4', 'max:50', 'regex:/^[a-zA-Z\s.-]+$/', 'unique:teachers.teacher_name'],
+            'teacher_name' => ['required', 'string', 'min:4', 'max:100', 'regex:/^[a-zA-Z\s.-]+$/', 'unique:teachers.teacher_name'],
             'email' => ['required', 'email:rfc,dns', 'max:50'],
             'contact' => ['required', 'string', 'max:11', 'digits:11'],
             'student_id' => ['nullable'],
